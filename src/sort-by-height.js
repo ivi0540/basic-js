0a84c8d25f91b66eda3c0eab85ffa3feab6ddd46
ivi0540 <ivi0540@gmail.com>
2023-12-22 13:20:00 +0300

    feat: problem sortByHeight solved

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -12,42 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
-  function insertElemInArr(arr, numPos) {
-    if (numPos === 0) {
-      return [-1, ...arr];
-    }
-    if (numPos === (arr.length - 1)) {
-      return [...arr, -1];
-    }
-    return [...arr.slice(0, numPos), -1, ...arr.slice(numPos)];
-  };
-
-  let coptArr = arr;
-
-  let positionOne = [];
-  coptArr.map((elem, index) => {
+  let arrWithOne = arr.map((elem) => {
     if (elem === -1) {
-      positionOne.push(index);
-    }
-  });
-
-  coptArr = coptArr.filter((elem) => {
-    if (elem !== -1) {
-      return true;
+      return elem;
     } else {
-      return false;
+      return null;
     }
   });
 
-  coptArr = coptArr.sort((second, first) => {
+  let arrWithNum = arr.filter((elem) => {
+    if (elem !== -1) {
+      return elem;
+    }
+  });
+
+  arrWithNum = arrWithNum.sort((second, first) => {
     return second - first;
   });
 
-
-  positionOne.map((elem) => {
-    coptArr = insertElemInArr(coptArr, elem)
+  let result = arrWithOne.map((elem) => {
+    if (elem) {
+      return elem;
+    } else {
+      return arrWithNum.shift();
+    }
   });
-  return coptArr;
+  return result;
 }
 
 module.exports = {
